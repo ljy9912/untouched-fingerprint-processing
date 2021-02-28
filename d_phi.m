021d7963eb46b461822056f3882c7a8a67f98818
heni-l <79203764+heni-l@users.noreply.github.com>
2021-02-28 10:38:53 +0800

    MCC

--- d_phi.m
+++ d_phi.m
@@ -1,8 +1,8 @@
 function theta = d_phi(theta1,theta2)
-    theta = theta1-theta2;
+    theta = theta1 - theta2;
     if theta < -pi
-        theta = theta+2*pi;
+        theta = theta + 2*pi;
     elseif theta > pi
-        theta = theta-2*pi;
+        theta = theta - 2*pi;
     end
 end
